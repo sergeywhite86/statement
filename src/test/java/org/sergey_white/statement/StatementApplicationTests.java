054ee733b0021d0e6c1bafbfa0794abcfaa8d5cd
Sergey White <sergey_white86@mail.ru>
2024-12-10 13:15:05 +0300

    init commit

---
+++ src/test/java/org/sergey_white/statement/StatementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sergey_white.statement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
